cf94ed2fffcf315eebb141f41dd20549d2ce2d29
Eben <maradebenezer@gmail.com>
2023-07-30 21:29:13 +0100

    fixing errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
@@ -16,5 +17,13 @@ def terminate(self):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', debug=True, threaded=True)
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
