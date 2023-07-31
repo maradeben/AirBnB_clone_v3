1fa4d7e8839adf1a6664d88d17cb82046e5f7d1e
Eben <maradebenezer@gmail.com>
2023-07-31 21:40:27 +0100

    fixing 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ This module contains the first version of the API """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -20,7 +20,7 @@ def terminate(self):
 @app.errorhandler(404)
 def page_not_found(error):
     """ 404 error handler"""
-    return jsonify({"error": "not found"})
+    return make_response(jsonify({"error": "not found"}), 404)
 
 
 if __name__ == '__main__':
