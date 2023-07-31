0263ee620dd1450468563425d8eb71f22f670185
Eben <maradebenezer@gmail.com>
2023-07-31 21:31:56 +0100

    added 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ This module contains the first version of the API """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def terminate(self):
     """ the teardown context """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """ 404 error handler"""
+    return jsonify({"error": "not found"})
 
 
 if __name__ == '__main__':
